02d432186ddb7a89b8bec91b242e1a6c8e2ff9bf
Glen Young <extractofcactus@icloud.com>
2017-06-09 15:57:57 +0100

    all instance updates added

--- models/customer.rb
+++ models/customer.rb
@@ -2,6 +2,7 @@ require_relative('../db/sql_runner')
 
 class Customer
   attr_reader :id
+  attr_accessor :name, :funds
 
   def initialize (options)
     @id = options['id'].to_i if options['id']
@@ -11,9 +12,20 @@ class Customer
 
   def save()
     sql = "INSERT INTO customers (name, funds )
-     VALUES ( '#{@name}', '#{@funds}' ) RETURNING id"
+     VALUES ( '#{@name}', #{@funds} ) RETURNING id"
      customer = SqlRunner.run(sql).first()
      @id = customer['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE customers SET (
+    name,
+    funds
+    ) = (
+    '#{@name}',
+    #{@funds}
+    ) WHERE id = #{@id}"
+    SqlRunner.run(sql)
   end
 
 end
--- models/film.rb
+++ models/film.rb
@@ -2,6 +2,7 @@ require_relative('../db/sql_runner')
 
 class Film
   attr_reader :id
+  attr_accessor :title, :price
 
   def initialize (options)
     @id = options['id'].to_i if options['id']
@@ -11,9 +12,20 @@ class Film
 
   def save()
     sql = "INSERT INTO films (title, price )
-     VALUES ( '#{@title}', '#{@price}' ) RETURNING id"
+     VALUES ( '#{@title}', #{@price} ) RETURNING id"
      film = SqlRunner.run(sql).first()
      @id = film['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE films SET (
+    title,
+    price
+    ) = (
+    '#{@title}',
+    #{@price}
+    ) WHERE id = #{@id}"
+    SqlRunner.run(sql)
   end
 
 end
